d66bef70d67f7cbc77310ceff338d4ef83fc68a2
CMacSwam <c-mccluskey.1@onu.edu>
2024-10-24 08:53:50 -0400

    Completed countVowel function.

--- Project8-1/Project8-1.cpp
+++ Project8-1/Project8-1.cpp
@@ -6,16 +6,27 @@
 
 #include <iostream>
 #include <string>
+#include<cstring>
 
 using namespace std;
 
-int countCharacter(string str);
+int countVowel(string str);
 
 int main() {
 	
 
 }
 
-int countCharacter(string str) {
-	return str.length();
+int countVowel(string str) {
+
+	int count = 0;
+
+	for (int i = 0; i <= str.length(); i++) {
+
+		if (str[i] == 'a' || str[i] == 'e' || str[i] == 'o' || str[i] == 'i' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'O' || str[i] == 'I' || str[i] == 'U') {
+			count++;
+		}
+	}
+
+	return count;
 }
