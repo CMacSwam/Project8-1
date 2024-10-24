ec7437893d97a7b4667b2103ca1de6aef26a0091
CMacSwam <c-mccluskey.1@onu.edu>
2024-10-24 08:16:04 -0400

    Completed countCharacter function

--- Project8-1/Project8-1.cpp
+++ Project8-1/Project8-1.cpp
@@ -3,3 +3,19 @@
 * 
 * 
 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int countCharacter(string str);
+
+int main() {
+	
+
+}
+
+int countCharacter(string str) {
+	return str.length();
+}
